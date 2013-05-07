778a4b79c42f8a8739b2d0de7d0471f3880ec73f
Justin McAdams <mcadamsrails@gmail.com>
2013-05-06 23:07:47 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Zebra::Application.config.secret_token = '39b823afd714872b49036a7a2bd79dda550ec436f8497bc2b1c768523edb7c3ce690bffa53beb1b8992225539d750b1e5817df2ef48d0bcc107cca074487d9a9'
